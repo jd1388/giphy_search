ec782056c5fa7360c4d372c7bf7f340901cc7038
Jared Griffin <griffinjared1388@gmail.com>
2018-03-27 13:18:06 -0500

    Converted into stateful component

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Screen from '../enum/Screen';
 
-const Trending = props => {
-    const { screen } = props;
+export default class Trending extends Component {
+    render() {
+        const { screen } = this.props;
 
-    if (screen !== Screen.trending)
-        return <div/>;
+        if (screen !== Screen.trending)
+            return <div/>;
 
-    return (
-        <div>Hello, I'm trending!!!</div>
-    );
-};
-
-export default Trending;
+        return (
+            <div>Hello, I'm trending!!!</div>
+        );
+    }
+}
