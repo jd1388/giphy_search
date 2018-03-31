6c2a7c72b592e19985978ac1eff3f7f07c228d38
Jared Griffin <griffinjared1388@gmail.com>
2018-03-30 22:35:04 -0500

    Added animation to gif when clicked

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image } from 'semantic-ui-react';
+import { Image, Transition } from 'semantic-ui-react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import Styles from '../styles/components/Gif';
@@ -9,24 +9,33 @@ export default class Gif extends Component {
         super(props);
 
         this.state = {
-            url: props.gifUrl.still
+            url: props.gifUrl.still,
+            visible: true
         };
     }
 
     render() {
         const { native, gifUrl, style, displayMessage } = this.props;
         const { still, gif, source } = gifUrl;
-        const { url } = this.state;
+        const { url, visible } = this.state;
 
         return (
-            <CopyToClipboard text={source} onCopy={displayMessage}>
-                <Image
-                    src={url}
+            <Transition animation='pulse' duration={500} visible={visible}>
+                <CopyToClipboard
                     style={Object.assign({}, Styles.gif, style)}
-                    onMouseOver={() => this.setState({ url: native ? source : gif })}
-                    onMouseOut={() => this.setState({ url: still })}
-                />
-            </CopyToClipboard>
+                    text={source}
+                    onCopy={() => {
+                        this.setState({ visible: !visible });
+                        displayMessage();
+                    }}
+                >
+                    <Image
+                        src={url}
+                        onMouseOver={() => this.setState({ url: native ? source : gif })}
+                        onMouseOut={() => this.setState({ url: still })}
+                    />
+                </CopyToClipboard>
+            </Transition>
         );
     }
 }
