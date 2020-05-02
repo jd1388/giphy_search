9e307a339e9e7cc2724301a6d8d51956aea6b481
Jared Griffin <griffinjared1388@gmail.com>
2020-05-02 17:07:24 -0500

    Moved App.svelte into a components directory

---
+++ src/components/app/App.svelte
@@ -0,0 +1,8 @@
+<main>
+    <h1>Hello Jared!</h1>
+    <p>
+        Visit the
+        <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+        to learn how to build Svelte apps.
+    </p>
+</main>
