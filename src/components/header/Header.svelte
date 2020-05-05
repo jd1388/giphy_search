d0dbeb49b74bdeae16cbc08c4894284d09080483
Jared Griffin <griffinjared1388@gmail.com>
2020-05-05 06:44:07 -0500

    Began styling the header

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -3,14 +3,22 @@
     import { search } from 'svelte-awesome/icons'
 </script>
 
-<input
-    data-testid="search-bar"
-    placeholder="Search..."
-    type="search"
-/>
-<button
-    aria-label="search"
-    data-testid="search-button"
->
-    <Icon data={search} />
-</button>
+<div class="header-container">
+    <input
+        data-testid="search-bar"
+        placeholder="Search..."
+        type="search"
+    />
+    <button
+        aria-label="search"
+        data-testid="search-button"
+    >
+        <Icon data={search} />
+    </button>
+</div>
+
+<style>
+    .header-container {
+        background-color: var(--black);
+    }
+</style>
