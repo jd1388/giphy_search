94e994dc103a709df0b00939c8913dd9e0ca23b9
Jared Griffin <griffinjared1388@gmail.com>
2020-05-08 06:39:35 -0500

    Added test content

--- src/components/content/Content.svelte
+++ src/components/content/Content.svelte
@@ -1,1 +1,4 @@
 <div data-testid="content" />
+<p>Trending</p>
+<p>Random</p>
+<p>Search</p>
--- src/components/content/Content.test.js
+++ src/components/content/Content.test.js
@@ -4,9 +4,30 @@ import Content from './Content.svelte';
 
 describe('Content', () => {
     it('renders', () => {
-        const {getByTestId} = render(Content);
+        const { getByTestId } = render(Content);
         const contentContainer = getByTestId('content');
 
         expect(contentContainer).toBeInTheDocument();
+    });
+
+    it('displays trending', () => {
+        const { getByText } = render(Content);
+        const trendingContainer = getByText('Trending');
+
+        expect(trendingContainer).toBeVisible();
+    });
+
+    it('displays random', () => {
+        const { getByText } = render(Content);
+        const randomContainer = getByText('Random');
+
+        expect(randomContainer).toBeVisible();
+    });
+
+    it('displays search', () => {
+        const { getByText } = render(Content);
+        const searchContainer = getByText('Search');
+
+        expect(searchContainer).toBeVisible();
     });
 });
