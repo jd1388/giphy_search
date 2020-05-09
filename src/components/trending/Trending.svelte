340a90390f32a723151b5e20ec87bd28183ab899
Jared Griffin <griffinjared1388@gmail.com>
2020-05-09 10:29:19 -0500

    Display trending gifs

--- src/components/trending/Trending.svelte
+++ src/components/trending/Trending.svelte
@@ -1,1 +1,58 @@
-<div data-testid="trending-container" />
+<script>
+    import { getTrendingGifs } from '../../services/giphy-service';
+
+    const trendingGifsPromise = getTrendingGifs();
+</script>
+
+<div
+    class="trending-container"
+    data-testid="trending-container"
+>
+    <div class="trending-content">
+        <h1>Trending</h1>
+        <hr>
+        {#await trendingGifsPromise}
+            <div data-testid="loading-trending">
+                Loading
+            </div>
+        {:then trendingGifs}
+            <div class="gifs-container">
+                {#each trendingGifs as trendingGif}
+                    <img
+                        alt="trending gif"
+                        class="gif"
+                        data-testid="trending-gif"
+                        src={trendingGif.still}
+                    >
+                {/each}
+            </div>
+        {/await}
+    </div>
+</div>
+
+<style>
+    .trending-container {
+        display: grid;
+        grid-template-areas: ". trending-content .";
+        grid-template-columns: 1fr 3fr 1fr;
+    }
+
+    .trending-content {
+        grid-area: trending-content;
+    }
+
+    .gifs-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    .gifs-container::after {
+        content: "";
+        flex: auto;
+    }
+
+    .gif {
+        padding: 4px;
+    }
+</style>
---
+++ src/components/trending/Trending.test.js
@@ -0,0 +1,50 @@
+import Chance from 'chance';
+import { render } from '@testing-library/svelte';
+
+import { getTrendingGifs } from '../../services/giphy-service';
+
+import Trending from './Trending.svelte';
+
+jest.mock('../../services/giphy-service');
+
+describe('Trending', () => {
+    const chance = new Chance();
+    const createFakeGif = () => ({
+        gif: chance.url(),
+        source: chance.url(),
+        still: chance.url() 
+    });
+    const fakeGifs = chance.n(createFakeGif, 50);
+
+    getTrendingGifs.mockResolvedValue(fakeGifs);
+
+    it('should display a "Trending" header', () => {
+        const { getByText } = render(Trending);
+        const trendingHeader = getByText('Trending');
+
+        expect(trendingHeader).toBeVisible();
+    });
+
+    it('should display a loading spinner while loading gifs', () => {
+        const loadingGifs = new Promise((resolve) => {
+            setTimeout(resolve, 10000)
+        });
+        getTrendingGifs.mockResolvedValueOnce(loadingGifs);
+
+        const { getByTestId } = render(Trending);
+        const loadingSpinner = getByTestId('loading-trending');
+
+        expect(loadingSpinner).toBeVisible();
+    });
+
+    it('should display the trending gifs after loading', async () => {
+        const { findAllByTestId, queryByTestId } = render(Trending);
+        const trendingGifs = await findAllByTestId('trending-gif');
+        const loadingSpinner = queryByTestId('loading-trending');
+
+        trendingGifs.forEach(trendingGif => {
+            expect(trendingGif).toBeVisible();
+        });
+        expect(loadingSpinner).not.toBeInTheDocument();
+    });
+});
