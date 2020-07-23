af3d8ad05472cda44f8365fec82b99668db1558f
Jared Griffin <jgriffin@sourceallies.com>
2020-07-23 17:37:20 -0500

    Added a helper function for getting gifs for a search term

--- src/services/giphy-service/GiphyService.js
+++ src/services/giphy-service/GiphyService.js
@@ -9,15 +9,25 @@ export const getTrendingGifsOld = () => {
     return Promise.resolve(testGifs);
 };
 
+const mapGifDataToGifObject = gifData => ({
+    gif: gifData.images.fixed_height_small.url,
+    source: gifData.images.original.url,
+    still: gifData.images.fixed_height_small_still.url,
+    title: gifData.title
+});
+
 export const getTrendingGifs = async () => {
     const trendingRoute = 'https://api.giphy.com/v1/gifs/trending?api_key=rgA4LYZjZAvQy5xlghCw1oJ6mnkAQ4HJ&limit=100';
     const trendingGifsResponse = await fetch(trendingRoute);
     const trendingGifsData = await trendingGifsResponse.json();
     
-    return trendingGifsData.data.map(trendingGifData => ({
-        gif: trendingGifData.images.fixed_height_small.url,
-        source: trendingGifData.images.original.url,
-        still: trendingGifData.images.fixed_height_small_still.url,
-        title: trendingGifData.title
-    }));
+    return trendingGifsData.data.map(mapGifDataToGifObject);
+};
+
+export const getGifsForSearchTerm = async (searchTerm) => {
+    const searchRoute = `https://api.giphy.com/v1/gifs/search?api_key=rgA4LYZjZAvQy5xlghCw1oJ6mnkAQ4HJ&q=${searchTerm}&limit=100`;
+    const searchGifsResponse = await fetch(searchRoute);
+    const searchGifsData = await searchGifsResponse.json();
+    
+    return searchGifsData.data.map(mapGifDataToGifObject);
 };
--- src/services/giphy-service/GiphyService.test.js
+++ src/services/giphy-service/GiphyService.test.js
@@ -1,8 +1,10 @@
 import Chance from 'chance';
 
-import { getTrendingGifs } from './GiphyService';
+import { getTrendingGifs, getGifsForSearchTerm } from './GiphyService';
 
 describe('Giphy Service', () => {
+    global.fetch = jest.fn();
+
     const chance = new Chance();
     const createRandomGifData = () => ({
         images: {
@@ -19,27 +21,26 @@ describe('Giphy Service', () => {
         title: chance.sentence()
     });
 
-    global.fetch = jest.fn();
+    const fakeGifsData = {
+        data: chance.n(createRandomGifData, 50)
+    };
+    const getFakeGifs = () => Promise.resolve(fakeGifsData);
+    const fakeFetchResponse = Promise.resolve({
+        json: getFakeGifs
+    });
+
+    beforeEach(() => {
+        fetch.mockClear();
+        fetch.mockResolvedValueOnce(fakeFetchResponse);
+    });
 
     describe('getTrendingGifs', () => {
-        const fakeGifsData = {
-            data: chance.n(createRandomGifData, 50)
-        };
-        const getFakeGifs = () => Promise.resolve(fakeGifsData);
-        const fakeFetchResponse = Promise.resolve({
-            json: getFakeGifs
-        });
-
-        beforeEach(() => {
-            global.fetch.mockResolvedValueOnce(fakeFetchResponse);
-        });
-
         it('gets the top 100 trending gifs from giphy', async () => {
             await getTrendingGifs();
 
             const expectedGiphyTrendingRoute = 'https://api.giphy.com/v1/gifs/trending?api_key=rgA4LYZjZAvQy5xlghCw1oJ6mnkAQ4HJ&limit=100';
 
-            expect(global.fetch).toHaveBeenCalledWith(expectedGiphyTrendingRoute);
+            expect(fetch).toHaveBeenCalledWith(expectedGiphyTrendingRoute);
         });
 
         it('returns the trending gifs', async () => {
@@ -50,6 +51,30 @@ describe('Giphy Service', () => {
                 title: gifData.title
             }));
             const actualGifs = await getTrendingGifs();
+
+            expect(actualGifs).toEqual(expectedGifs);
+        });
+    });
+
+    describe('getGifsForSearchTerm', () => {
+        const fakeSearchTerm = chance.word();
+
+        it('gets the first 100 results for the search term', async () => {
+            await getGifsForSearchTerm(fakeSearchTerm);
+
+            const expectedGiphySearchRoute = `https://api.giphy.com/v1/gifs/search?api_key=rgA4LYZjZAvQy5xlghCw1oJ6mnkAQ4HJ&q=${fakeSearchTerm}&limit=100`;
+
+            expect(fetch).toHaveBeenCalledWith(expectedGiphySearchRoute);
+        });
+
+        it('returns the gifs matching the search term', async () => {
+            const expectedGifs = fakeGifsData.data.map(gifData => ({
+                gif: gifData.images.fixed_height_small.url,
+                source: gifData.images.original.url,
+                still: gifData.images.fixed_height_small_still.url,
+                title: gifData.title
+            }));
+            const actualGifs = await getGifsForSearchTerm(fakeSearchTerm);
 
             expect(actualGifs).toEqual(expectedGifs);
         });
